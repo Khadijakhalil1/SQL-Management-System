b528aad06e3ea997c7e42b83eefd8cd8c2ac1ed5
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-05-09 09:17:52 +0500

    task3

---
+++ Practice/Booking.sql
@@ -0,0 +1,8 @@
+select*from task3
+insert into task3(hotelNo,guestNo,dateFrom,dateTo,roomNo)Values('2004','1234','jan','july','4411');
+insert into task3(hotelNo,guestNo,dateFrom,dateTo,roomNo)Values('1111','8244','july','aug','7688');
+insert into task3(hotelNo,guestNo,dateFrom,dateTo,roomNo)Values('2233','3552','aug','NULL','4566');
+insert into task3(hotelNo,guestNo,dateFrom,dateTo,roomNo)Values('6345','3534','april','aug','2345');
+insert into task3(hotelNo,guestNo,dateFrom,dateTo,roomNo)Values('2003','7656','may','NULL','8973');
+insert into task3(hotelNo,guestNo,dateFrom,dateTo,roomNo)Values('3344','5646','NULL','aug','9732');
+insert into task3(hotelNo,guestNo,dateFrom,dateTo,roomNo)Values('1976','6532','aug','sep','9873');
